d6388ab3a5db102f0ad832e8ae2206243c97cc2d
Tomás <tomas_ss@aol.com>
2023-10-04 15:13:06 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
+
     const heroSection = document.querySelector('.hero');
     const alturaHero = heroSection.clientHeight;
-    
+
     window.addEventListener('scroll', function() {
         const posicaoAtual = window.scrollY;
 
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 
-    // Seção das atrações, programação das abas
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -23,24 +23,24 @@ document.addEventListener('DOMContentLoaded', function() {
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
-            botao.target.classList.add('shows__tabs__button--is-active')
+            botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
 
     // Seção FAQ, accordion
     for (let i = 0; i < questions.length; i++) {
-        questions[i].addEventListener('click', abreOuFechaResposta)
+        questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
 
 function ocultaElementosDoHeader() {
     const header = document.querySelector('header');
-    header.classList.add('header--is-hidden')
+    header.classList.add('header--is-hidden');
 }
 
 function exibeElementosDoHeader() {
     const header = document.querySelector('header');
-    header.classList.remove('header--is-hidden')
+    header.classList.remove('header--is-hidden');
 }
 
 function abreOuFechaResposta(elemento) {
@@ -52,9 +52,9 @@ function abreOuFechaResposta(elemento) {
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active') 
+        buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
 
